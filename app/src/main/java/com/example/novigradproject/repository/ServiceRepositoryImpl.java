488e90eb59f0e2910af3440558aba134ecc81140
Joaquin144 <vvpp898.comco@gmail.com>
2023-11-09 19:02:19 +0530

    checkpoint 1

--- app/src/main/java/com/example/novigradproject/repository/ServiceRepositoryImpl.java
+++ app/src/main/java/com/example/novigradproject/repository/ServiceRepositoryImpl.java
@@ -1,4 +1,20 @@
 package com.example.novigradproject.repository;
 
+import com.example.novigradproject.model.user.Person;
+
 public class ServiceRepositoryImpl implements ServiceRepository {
+    @Override
+    public void createService(Person person) {
+
+    }
+
+    @Override
+    public void modifyService(Person person) {
+
+    }
+
+    @Override
+    public void deleteService(Person person) {
+
+    }
 }
